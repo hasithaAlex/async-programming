d4062c36e4fc01b22d7a6d01dcb9db698b451073
hasitha <hasithamadusanka90@gmail.com>
2018-03-11 19:40:14 +0530

    first commit

---
+++ 006TestingDelegates/TestDelegates.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace _006TestingDelegates
+{
+    [TestClass]
+    public class TestDelegates
+    {
+        private void DoWork()
+        {
+            Debug.WriteLine("Hello Test Case");
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DoWork();
+        }
+    }
+}
